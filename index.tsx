d5b06ea94b01032274e90d168a65d81bf478ccb3
Shane Louise Bustamante <123712391+BustamanteSLB@users.noreply.github.com>
2025-04-17 22:37:06 +0800

    Delete index.tsx

--- index.tsx
+++
@@ -1,7 +0,0 @@
-import { Link, Redirect } from "expo-router";
-import { Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
-
-export default function App() {
-  return <Redirect href="/(auth)/signin"/>
-}

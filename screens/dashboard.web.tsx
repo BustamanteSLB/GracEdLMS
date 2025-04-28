a5388a34c316b6864a6f56a5f078317806317c5e
Shane Louise Bustamante <123712391+BustamanteSLB@users.noreply.github.com>
2025-04-29 03:55:32 +0800

    Added dark mode functionality

--- screens/dashboard.web.tsx
+++ screens/dashboard.web.tsx
@@ -1,13 +1,18 @@
-import { Text, View } from 'react-native'
+import { Text, useColorScheme, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { useDarkMode } from '../contexts/DarkModeContext';
 
 const DBWeb: React.FC = () => {
+
+  const colorScheme = useColorScheme();
+  const { isDarkMode } = useDarkMode();
+
   return(
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-inter_bold">Dashboard - Web</Text>
-      <StatusBar style="auto"/>
+    <SafeAreaView className={`flex-1 items-center justify-center ${isDarkMode ? 'bg-[#121212]' : 'bg-white'}`}>
+      <Text className={`text-3xl font-inter_bold ${isDarkMode ? 'text-[#E0E0E0]' : 'text-black'}`}>Dashboard - Web</Text>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} backgroundColor={colorScheme === 'dark' ? 'dark' : 'light'} />
     </SafeAreaView>
   )
 }

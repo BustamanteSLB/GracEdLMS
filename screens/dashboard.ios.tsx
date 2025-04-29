4ca4bb836835889e6d84175b75fb7762a8526a07
Shane Louise Bustamante <123712391+BustamanteSLB@users.noreply.github.com>
2025-04-29 22:06:09 +0800

    Update dashboard.ios.tsx

--- screens/dashboard.ios.tsx
+++ screens/dashboard.ios.tsx
@@ -1,8 +1,9 @@
-import { Text, TouchableOpacity, useColorScheme, View } from 'react-native'
+import { Dimensions, Text, TouchableOpacity, useColorScheme, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { FlashList } from '@shopify/flash-list'
 import { Image } from "expo-image";
 import { cssInterop } from "nativewind";
+import { useState, useEffect } from 'react';
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { useRouter } from 'expo-router';
@@ -38,13 +39,38 @@ const DBIOS: React.FC = () => {
     },
   ];
 
+  const [isPortrait, setIsPortrait] = useState(true);
+
+  
+  useEffect(() => {
+    const updateOrientation = () => {
+      const { width, height } = Dimensions.get('window');
+      setIsPortrait(height >= width);
+    };
+    
+    const subscription = Dimensions.addEventListener('change', updateOrientation);
+    
+    // Initial check
+    updateOrientation();
+    
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   return (
     <SafeAreaView className={`flex-1 items-center justify-center ${isDarkMode ? 'bg-[#121212]' : 'bg-white'}`}>
       <Text className={`text-left self-start font-inter_semibold ml-[10px] ${isDarkMode ? 'text-[#E0E0E0]' : 'text-black'}`}>Welcome, student.</Text>
       <FlashList
         data={data}
         renderItem={({ item }) => (
-          <TouchableOpacity className={`min-h-[125px] rounded-[10px] flex-row m-[10px] p-[10px] ${isDarkMode ? 'bg-[#1E1E1E] shadow-none' : 'bg-white elevation-xl shadow-black'}`} onPress={item.onPress}>
+          <TouchableOpacity 
+            className={
+              `min-h-[125px] rounded-[10px] flex-row m-[12px] p-[12px]  
+              ${isPortrait ? 'w-auto' : 'w-[300px]'}
+              ${isDarkMode ? 'bg-[#1E1E1E] shadow-none' : 'bg-white elevation-xl shadow-black'}`
+            } 
+            onPress={item.onPress}>
             <View>
               <Image
                 source={item.image}
@@ -62,9 +88,10 @@ const DBIOS: React.FC = () => {
         )}
         estimatedItemSize={10}
         estimatedListSize={{height: 100, width: 400}}
+        horizontal={!isPortrait}
         keyExtractor={(item, index) => index.toString()}
       />
-      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} backgroundColor={colorScheme === 'dark' ? 'dark' : 'light'} />
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} backgroundColor={colorScheme === 'dark' ? 'black' : 'white'}/>
     </SafeAreaView>
   )
 }

412e8f14a6299434081fd591f7f18e3bb8e561b7
Shane Louise Bustamante <123712391+BustamanteSLB@users.noreply.github.com>
2025-05-31 20:28:54 +0800

    Update _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack } from "expo-router";
 export default function AuthLayout() {
   return (
     <Stack>
+      <Stack.Screen name="register" options={{ headerShown: false }}/>
       <Stack.Screen name="signin" options={{ headerShown: false }}/>
     </Stack>
   )
